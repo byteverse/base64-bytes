6454d3971e55988e8c9fe279de3eedccdebace21
Andrew Martin <andrew.thaddeus@gmail.com>
2022-06-16 12:56:59 -0400

    Add decode64

--- src/Data/Bytes/Base64.hs
+++ src/Data/Bytes/Base64.hs
@@ -14,6 +14,7 @@ module Data.Bytes.Base64
   ( encode
   , builder
   , recodeBoundedBuilder
+  , decode64
   ) where
 
 import GHC.TypeNats (type (+),type (*),Div)
@@ -24,17 +25,19 @@ import Data.Bytes.Types (Bytes(Bytes))
 import Data.Primitive (ByteArray(..),MutableByteArray(..))
 import Data.Primitive (newByteArray,unsafeFreezeByteArray,readByteArray)
 import Data.Primitive.Ptr (indexOffPtr)
-import Data.Word (Word8)
+import Data.Word (Word8,Word64)
 import GHC.Exts (Ptr(Ptr),Int(I#),State#,(+#),(-#))
 import GHC.ST (ST(ST))
 import GHC.Word (Word(W#),Word32(W32#))
 
 import qualified Arithmetic.Nat as Nat
 import qualified Arithmetic.Types as Arithmetic
+import qualified Data.Bytes as Bytes
 import qualified Data.Bytes.Builder.Unsafe as BU
 import qualified Data.Bytes.Builder.Bounded.Unsafe as BBU
 import qualified Data.Primitive.ByteArray.BigEndian as BE
 import qualified Data.Primitive.ByteArray.LittleEndian as LE
+import qualified Data.Primitive.Ptr as PM
 import qualified GHC.Exts as Exts
 
 -- | Encode a byte sequence with base64.
@@ -211,3 +214,34 @@ c2w = fromIntegral . ord
 
 unST :: ST s a -> State# s -> (# State# s, a #)
 unST (ST f) s = f s
+
+-- Decode a base64-url-encoded 64-bit word. Rejects encoded numbers
+-- greater than or equal to @2^64@. This maps the rightmost byte to
+-- the 6 least significant bits of the word.
+decode64 :: Bytes -> Maybe Word64
+decode64 bs
+  | Bytes.length bs > 10 = Nothing
+  | otherwise = Bytes.foldlM
+      (\ !(acc :: Word64) b -> case PM.indexOffPtr decodeTable (fromIntegral @Word8 @Int b) of
+        0xFF -> Nothing
+        w -> pure $! (unsafeShiftL acc 6 .|. fromIntegral @Word8 @Word64 w)
+      ) 0 bs
+
+decodeTable :: Ptr Word8
+decodeTable = Ptr
+  "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+  \\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+  \\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x3e\xff\xff\xff\x3f\
+  \\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\xff\xff\xff\x63\xff\xff\
+  \\xff\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\
+  \\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\xff\xff\xff\xff\xff\
+  \\xff\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\
+  \\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\xff\xff\xff\xff\xff\
+  \\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+  \\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+  \\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+  \\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+  \\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+  \\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+  \\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+  \\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"#
